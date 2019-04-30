93590c7908ae6f919f84e1c0dba7d916545cad54
Dumagana <ariel.dumagan@gmail.com>
2019-04-30 16:51:06 -0500

    Add files via upload

---
+++ PackageExample/src/CircleApp.java
@@ -0,0 +1,10 @@
+import edu.lewisu.cs.shapes.Circle;
+
+public class CircleApp {
+
+	public static void main(String[] args) {
+		Circle c = new Circle();
+
+	}
+
+}
---
+++ PackageExample/src/edu/lewisu/cs/shapes/Circle.java
@@ -0,0 +1,9 @@
+package edu.lewisu.cs.shapes;
+
+public class Circle {
+	@Override 
+	public String toString() {
+		return "I am a circle";
+	}
+		
+}
